2aad7275fdf169b785690bff3deaaf439ba3b3d4
Alexandre Stachnik <astachni@z3r6p3.42lyon.fr>
2023-03-03 17:36:31 +0100

    a

--- client.c
+++ client.c
@@ -1,39 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   server.c                                           :+:      :+:    :+:   */
+/*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: astachni <astachni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/27 15:41:01 by astachni          #+#    #+#             */
-/*   Updated: 2023/03/03 16:04:33 by astachni         ###   ########.fr       */
+/*   Created: 2023/03/03 15:08:42 by astachni          #+#    #+#             */
+/*   Updated: 2023/03/03 16:05:53 by astachni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header/minitalk.h"
 
-void	signal_handler(int sign)
+int	main(int ac, char **av)
 {
-	ft_printf("\nsignal recu\n");
-}
-
-int	main(void)
-{
-	__pid_t	pid;
-
-	pid = getpid();
-	ft_printf("%d\n", pid);
-	struct	sigaction new_handler = 
-	{
-		.sa_handler = signal_handler
-	};
-	if (sigaction(SIGUSR1, &new_handler, NULL) < 0)
-	{
-		ft_printf("--%d--", sigaction(SIGUSR1, &new_handler, NULL));
-	}
-	while (1)
-	{
-		pause();
-	}
+	kill(ft_atoi(av[1]), getpid());
+	sleep(1);
+	kill(ft_atoi(av[1]), SIGUSR1);
 	return (0);
 }
